cf630947867575dfeba92df8aceca2333907e87c
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-07-01 23:48:48 +0300

    Update GeminiService.java

--- src/main/java/com/example/dermAI/geminiservice/GeminiService.java
+++ src/main/java/com/example/dermAI/geminiservice/GeminiService.java
@@ -1,5 +1,7 @@
 package com.example.dermAI.geminiservice;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,7 +19,7 @@ public class GeminiService {
 
     public GeminiService(WebClient.Builder builder) {
         this.webClient = builder
-                .baseUrl("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent")
+                .baseUrl("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent")
                 .build();
     }
 
@@ -30,11 +32,33 @@ public class GeminiService {
                 )
         );
 
-        return webClient.post()
-                .uri(uriBuilder -> uriBuilder.queryParam("key", apiKey).build())
-                .bodyValue(body)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        try {
+            String rawResponse = webClient.post()
+                    .uri(uriBuilder -> uriBuilder.queryParam("key", apiKey).build())
+                    .bodyValue(body)
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            System.out.println("🎯 RAW RESPONSE:");
+            System.out.println(rawResponse);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(rawResponse);
+
+            return jsonNode
+                    .path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+
+        } catch (Exception e) {
+            System.err.println("❌ GeminiService HATASI: " + e.getMessage());
+            return "Hata oluştu: " + e.getMessage();
+        }
+
+
     }
 }
